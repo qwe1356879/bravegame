4009aec35e2991c7451dbfeb1369ea735787f650
FanR <62733305+qwe1356879@users.noreply.github.com>
2022-06-08 12:34:24 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ const { resolve } = require('path')
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+   publicPath:"./", // 可以设置成相对路径，这样所有的资源都会被链接为相对路径，打出来的包可以被部署在任意路径
+    outputDir:"dist", //打包时生成的生产环境构建文件的目录
+    assetsDir: 'static', // 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
+    runtimeCompiler: true,
   // server: {
   //   post:"0.0.0.0",
   //   port: '3000',//端口号
